1c7a1603885c5d3aa3be39debe70ad5f917e0a5d
webappstudioz <webappstudioz12@gmail.com>
2024-07-31 01:32:08 +0530

    copy to clipboard implemented

--- src/pages/Accounts/depositFunds.js
+++ src/pages/Accounts/depositFunds.js
@@ -32,6 +32,7 @@ import TetherLogo from "../../assets/images/c2c/tetherlogo.png"
 import file from "../../assets/images/file.png";
 import { adminAccountsDetails, handlePayents } from "../Authentication/store/apiServices"
 import { useHistory } from "react-router-dom"
+import copy from "copy-to-clipboard"
 
 const DepositFunds = () => {
   let navigate = useHistory()
@@ -74,34 +75,34 @@ const DepositFunds = () => {
 
     validationSchema: (selectedMethod) => {
       let schema = Yup.object().shape({
-      customAmount: Yup.string()
-        .required("Please enter amount.")
-        .matches(customRegex?.amount, "Please valid amount"),
-      paymentId: Yup.string().when('selectedMethod', {
-        is: "tether",
-        then: Yup.string()
-          .required("Please enter payment id"),
-      }),
-      upiId: Yup.string().when('selectedMethod', {
-        is: "bank",
-        then: Yup.string()
-          .required("Please enter your UPI ID"),
-      }),  
-    })
+        customAmount: Yup.string()
+          .required("Please enter amount.")
+          .matches(customRegex?.amount, "Please valid amount"),
+        paymentId: Yup.string().when('selectedMethod', {
+          is: "tether",
+          then: Yup.string()
+            .required("Please enter payment id"),
+        }),
+        upiId: Yup.string().when('selectedMethod', {
+          is: "bank",
+          then: Yup.string()
+            .required("Please enter your UPI ID"),
+        }),
+      })
 
-    return schema
-  },
+      return schema
+    },
     onSubmit: async (values) => {
       let data = new FormData()
-      if(selectedFile){
+      if (selectedFile) {
         data.append('image', selectedFile);
       }
 
-      if(values?.paymentId){
+      if (values?.paymentId) {
         data.append('payment_id', values?.paymentId);
       }
 
-      if(values.upiId){
+      if (values.upiId) {
         data.append('upi_id', values?.upiId)
       }
 
@@ -115,8 +116,8 @@ const DepositFunds = () => {
           setOpenModal(false)
           navigate.push("/dashboard")
           toast.success(result?.data?.message, {
-						position: toast.POSITION.TOP_RIGHT,
-					})
+            position: toast.POSITION.TOP_RIGHT,
+          })
         } catch (error) {
           toast.error(error?.response?.data?.message, {
             position: toast.POSITION.TOP_RIGHT,
@@ -351,8 +352,35 @@ const DepositFunds = () => {
                                       <td className="text-right">
                                         {adminInfo?.crypto_id}
                                       </td>
+                                      <td></td>
+                                      <td></td>
+                                      <td>
+                                        <svg
+                                          onClick={() => {
+                                            copy(adminInfo?.crypto_id);
+                                            toast("Crypto id has been copied", {
+                                              autoClose: 1000,
+                                            });
+                                          }}
+                                          className="pw-icon-pass"
+                                          width="13"
+                                          height="17"
+                                          viewBox="0 0 15 17"
+                                          fill="none"
+                                          xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                          <path
+                                            d="M2.02526 16.4685C1.8004 16.4153 1.58059 16.3427 1.36827 16.2515C1.07612 16.1087 0.830075 15.8867 0.65819 15.6106C0.486306 15.3346 0.395472 15.0156 0.396227 14.6905C0.379227 14.1735 0.396227 13.6564 0.396227 13.1394C0.396227 10.6354 0.396227 8.13178 0.396227 5.62845C0.405174 5.19207 0.572476 4.77374 0.866808 4.45144C1.16114 4.12915 1.56255 3.9249 1.99633 3.87649C2.45233 3.84249 2.91227 3.87039 3.38927 3.87039V4.00247C3.38927 6.21447 3.37827 8.42646 3.39427 10.6385C3.37907 11.1864 3.52846 11.7263 3.82311 12.1885C4.11775 12.6507 4.54409 13.014 5.04723 13.2315C5.42622 13.4071 5.84075 13.4926 6.25829 13.4815H12.3423C12.3423 13.8745 12.3483 14.2545 12.3423 14.6345C12.3431 14.7976 12.3219 14.96 12.2793 15.1175C12.1904 15.4667 11.9978 15.781 11.7268 16.0186C11.4558 16.2562 11.1191 16.4061 10.7612 16.4485C10.7428 16.453 10.725 16.4597 10.7082 16.4685H2.02526Z"
+                                            fill="#9F9EB2"
+                                          />
+                                          <path
+                                            d="M4.37758 6.50446C4.37758 5.13246 4.37758 3.76019 4.37758 2.38752C4.37009 2.09061 4.43648 1.79651 4.57069 1.53156C4.70491 1.26662 4.9028 1.03905 5.14662 0.869455C5.43858 0.650757 5.79381 0.533193 6.15858 0.534494C8.51192 0.530494 10.8646 0.530494 13.2166 0.534494C13.6825 0.543533 14.1271 0.731573 14.4583 1.0594C14.7894 1.38722 14.9818 1.8297 14.9956 2.29548C15.0056 3.36048 14.9956 4.42568 14.9956 5.49568C14.9956 7.20634 14.9956 8.91685 14.9956 10.6275C15.0066 11.0475 14.8709 11.4583 14.612 11.7891C14.353 12.12 13.9869 12.3502 13.5766 12.4405C13.4481 12.4706 13.3165 12.4854 13.1846 12.4844C10.8513 12.4844 8.51792 12.4844 6.18459 12.4844C5.70827 12.4847 5.25107 12.297 4.91249 11.962C4.57391 11.627 4.38135 11.1719 4.3766 10.6956C4.3686 9.29563 4.3766 7.90461 4.3766 6.50862L4.37758 6.50446Z"
+                                            fill="#9F9EB2"
+                                          />
+                                        </svg>
+                                      </td>
                                     </React.Fragment>
-                                    <><td></td><td></td><td></td></>
+                                    {/* <><td></td><td></td><td></td></> */}
                                   </tr>
                                 </tbody>
                               </table>
@@ -383,38 +411,38 @@ const DepositFunds = () => {
                             <div className="col-md-12">
                               <table className="w-100">
                                 <div className="col-lg-6 form-group payment-id-box" >
-                            <p className="place-holder">Payment id</p>
-                            <Input
-                              type="text"
-                              placeholder="Enter payment id"
-                              className="form-control"
-                              id="upiId"
-                              name="upiId"
-                              value={DepositForm?.values?.upiId || ""}
-                              onChange={DepositForm.handleChange}
-                              onBlur={DepositForm.handleBlur}
-                              invalid={
-                                DepositForm.touched.upiId &&
-                                  DepositForm.errors.upiId
-                                  ? true
-                                  : false
-                              }
-                            />
-                            {DepositForm.touched.upiId &&
-                              DepositForm.errors.upiId ? (
-                              <>
-                                <FormFeedback type="invalid">
-                                  <img
-                                    className="form-error-icon"
-                                    src={rederror}
-                                    alt=""
-                                    height={15}
+                                  <p className="place-holder">Payment id</p>
+                                  <Input
+                                    type="text"
+                                    placeholder="Enter payment id"
+                                    className="form-control"
+                                    id="upiId"
+                                    name="upiId"
+                                    value={DepositForm?.values?.upiId || ""}
+                                    onChange={DepositForm.handleChange}
+                                    onBlur={DepositForm.handleBlur}
+                                    invalid={
+                                      DepositForm.touched.upiId &&
+                                        DepositForm.errors.upiId
+                                        ? true
+                                        : false
+                                    }
                                   />
-                                  {DepositForm.errors.upiId}
-                                </FormFeedback>
-                              </>
-                            ) : null}
-                          </div>
+                                  {DepositForm.touched.upiId &&
+                                    DepositForm.errors.upiId ? (
+                                    <>
+                                      <FormFeedback type="invalid">
+                                        <img
+                                          className="form-error-icon"
+                                          src={rederror}
+                                          alt=""
+                                          height={15}
+                                        />
+                                        {DepositForm.errors.upiId}
+                                      </FormFeedback>
+                                    </>
+                                  ) : null}
+                                </div>
                               </table>
                             </div>
                             {/* <div className="col-md-6">
@@ -498,7 +526,7 @@ const DepositFunds = () => {
                           <span>The minimum deposit amount is 50t. All deposit below the limit will be lost.</span>
                           <span>Carefully check the address. The transaction will be lost if the address is incorrect.</span>
                         </div>
-                       
+
                         <h6 className="font16  font-semibold">
                           How it works
                         </h6>
@@ -506,7 +534,7 @@ const DepositFunds = () => {
                           <span>Copy the address, then go to your crypto waller application, paste the address, specify the deposit amount, and complete the transfer</span>
                           <span>Carefully check the address. The transaction will be lost if the address is incorrect.</span>
                         </div>
-                        
+
                         <Row>
                           <Col>
                             <div className="form-check-inline mt-20 w-100">
@@ -523,9 +551,9 @@ const DepositFunds = () => {
                                     DepositForm.handleChange(e)
                                     setcustompay(e.target.value)
                                   }}
-                                  onBlur={(e) => { 
-                                    DepositForm.handleBlur, 
-                                    custompay > 5000 ? DepositForm.values.customAmount = 5000 :custompay < 50? DepositForm.values.customAmount = 50 : null 
+                                  onBlur={(e) => {
+                                    DepositForm.handleBlur,
+                                      custompay > 5000 ? DepositForm.values.customAmount = 5000 : custompay < 50 ? DepositForm.values.customAmount = 50 : null
                                   }}
                                   invalid={
                                     DepositForm.touched.customAmount &&
@@ -536,7 +564,7 @@ const DepositFunds = () => {
                                   type="number"
                                   name="customAmount"
 
-                                  // disabled={spinner}
+                                // disabled={spinner}
                                 />
 
                                 {DepositForm.touched.customAmount &&
@@ -555,7 +583,7 @@ const DepositFunds = () => {
                                 ) : null}
                               </div>
                             </div>
-                            
+
 
                           </Col>
                         </Row>
@@ -563,7 +591,7 @@ const DepositFunds = () => {
                     </div>
                   </CardBody>
                 </Card>
-               {selectedMethod === "tether" && <Card className="m-0 mt-3">
+                {selectedMethod === "tether" && <Card className="m-0 mt-3">
                   <CardBody>
                     <Row>
                       <Col>
